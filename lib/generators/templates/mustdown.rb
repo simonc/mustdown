87b17dcef472db8989e574d55dd817d939ceb191
Simon COURTOIS <scourtois@cubyx.fr>
2012-11-03 17:49:37 +0100

    Adding the markdown:install generator

---
+++ lib/generators/templates/mustdown.rb
@@ -0,0 +1,25 @@
+Mustdown.configure do |config|
+
+  # Read more about available options on the Redcarpet github page:
+  # https://github.com/vmg/redcarpet
+
+  # Markdown extensions
+  # see: https://github.com/vmg/redcarpet#and-its-like-really-simple-to-use
+  #
+  # config.markdown_extensions = {
+  #   no_intra_emphasis:  true,
+  #   tables:             true,
+  #   fenced_code_blocks: true,
+  #   autolink:           true,
+  #   strikethrough:      true
+  # }
+
+  # HTML renderer options
+  # see: https://github.com/vmg/redcarpet#darling-i-packed-you-a-couple-renderers-for-lunch-
+  #
+  # config.renderer_options = {
+  #   no_styles:        true,
+  #   safe_links_only:  true
+  # }
+
+end
