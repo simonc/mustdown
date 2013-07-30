681c4b68e93dbea8ae744c52c2a2fb62cd9b3bab
Simon Courtois <scourtois+github@cubyx.fr>
2013-07-30 16:08:10 +0200

    Tuning the dummy app to remove the useless parts

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,12 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "active_resource/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
 Bundler.require
 require "mustdown"
@@ -53,13 +49,13 @@ module Dummy
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    # config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    # config.assets.version = '1.0'
   end
 end
 
