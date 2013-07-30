762429dea26097e2b0a690fc2e8987d427e29a4e
Simon Courtois <scourtois+github@cubyx.fr>
2013-07-30 15:06:12 +0200

    Adding some comments in the rails engine

--- lib/mustdown/engine.rb
+++ lib/mustdown/engine.rb
@@ -1,9 +1,11 @@
 module Mustdown
   class Engine < ::Rails::Engine
+    # Disable specs generation
     config.generators do |g|
       g.test_framework :rspec, view_specs: false
     end
 
+    # Register MustdownHelper as a Rails helper
     initializer 'mustdown.action_controller' do |app|
       ActiveSupport.on_load :action_controller do
         helper Mustdown::MustdownHelper
